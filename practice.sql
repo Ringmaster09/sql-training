cec6fd960e309ce9f49aec6f86e8120293f53ef4
Ringmaster09 <priyanshchaudhary1@gmail.com>
2025-06-14 17:01:42 +0530

    Initial SQL practice upload

---
+++ practice.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE pet_shop;
+USE pet_shop;
+
+CREATE TABLE dogs (
+    name VARCHAR(50),
+    breed VARCHAR(90),
+    age INT
+);
+
+CREATE TABLE cats (
+    name VARCHAR(50),
+    age INT
+);
