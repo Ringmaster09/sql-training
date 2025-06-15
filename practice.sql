e61f1c6b17b7506129d54a48e2f0fea7a7530654
Ringmaster09 <priyanshchaudhary1@gmail.com>
2025-06-15 13:58:21 +0530

    practing

--- practice.sql
+++ practice.sql
@@ -11,3 +11,16 @@ CREATE TABLE cats (
     name VARCHAR(50),
     age INT
 );
+USE pet_shop;
+
+-- Insert into dogs table
+INSERT INTO dogs (name, breed, age) VALUES
+('Max', 'Labrador', 4),
+('Buddy', 'Beagle', 2),
+('Rocky', 'German Shepherd', 5);
+
+-- Insert into cats table
+INSERT INTO cats (name, age) VALUES
+('Whiskers', 3),
+('Mittens', 2);
+
